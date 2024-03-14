8be371622db462f661266879f7e486706632f804
viniciosbarbosa <viniciosbarbosagit@gmail.com>
2024-03-13 23:19:17 -0300

    instalndo biblioteca de libs

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
