eab825cd1a02e3fe1b8af0f4ff18c72eaae636c8
viniciosl <viniciosbarbosagit@gmail.com>
2024-03-23 16:14:10 -0300

    funcionalidade de logout

--- stock-control/src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ stock-control/src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,10 +1,22 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-toolbar-navigation',
   templateUrl: './toolbar-navigation.component.html',
   styleUrls: ['./toolbar-navigation.component.scss']
 })
 export class ToolbarNavigationComponent {
+
+  constructor(private cookieService:CookieService , private router:Router){
+
+  }
+
+  handleLogout(): void{
+    this.cookieService.delete('USER_INFO');
+    void this.router.navigate(['/home'])
+  }
+
+
 
 }
