b223ab4083a3f4c3610080a954ed3cdbe2d6a8aa
viniciosl <viniciosbarbosagit@gmail.com>
2024-05-11 23:14:08 -0300

    instalando material , criando tabela e refatorando toolbar

--- basic-knowledge/src/app/app.module.ts
+++ basic-knowledge/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
@@ -6,12 +8,29 @@ import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { GenderPipe } from "./pipes/gender.pipe";
 import { ToolbarComponent } from "./components/toolbar/toolbar.component";
-import { NewComponent } from './components/new/new.component';
-import { HomeComponent } from './components/home/home.component';
+import { NewComponent } from "./components/new/new.component";
+import { HomeComponent } from "./components/home/home.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
-  declarations: [AppComponent, GenderPipe, ToolbarComponent, NewComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    GenderPipe,
+    ToolbarComponent,
+    NewComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- basic-knowledge/src/app/components/home/home.component.ts
+++ basic-knowledge/src/app/components/home/home.component.ts
@@ -1,12 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+
+export interface Pessoa {
+  name: string;
+  gender: string;
+  age: string;
+  salary: string;
+}
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
-  people: Array<any> = [
+export class HomeComponent implements OnInit {
+  people: Array<Pessoa> = [
     {
       name: "vinicius",
       gender: "M",
@@ -21,9 +29,15 @@ export class HomeComponent {
     },
     {
       name: "youko",
-      gender: "",
+      gender: "F",
       age: "25",
       salary: "5000",
     },
   ];
+
+  displayedColumns: string[] = ["name", "gender", "age", "salary"];
+  dataSource!: MatTableDataSource<Pessoa>;
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.people);
+  }
 }
