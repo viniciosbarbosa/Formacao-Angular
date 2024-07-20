f0e4bb0933b542e00ffd33eabe2c5257ad14970b
viniciosl <viniciosbarbosagit@gmail.com>
2024-07-20 13:05:36 -0300

    filtro

--- v17/filter-user/src/app/components/components.module.ts
+++ v17/filter-user/src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
-  declarations: [UserDetailsComponent],
+  declarations: [UserDetailsComponent, FilterComponent],
   imports: [CommonModule, AngularMaterialModule],
-  exports: [UserDetailsComponent],
+  exports: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentsModule {}
