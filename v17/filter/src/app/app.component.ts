ce8755905819eb1cb436cccd58c0921eaea11b22
viniciosl <viniciosbarbosagit@gmail.com>
2025-02-06 16:55:52 -0300

    filtragem

--- v17/filter/src/app/app.component.ts
+++ v17/filter/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UserList } from './data/users-lists';
 import { IFilterOptions } from './interfaces/filter-options-interface';
-
+import { isWithinInterval } from 'date-fns';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,10 +10,11 @@ import { IFilterOptions } from './interfaces/filter-options-interface';
 })
 export class AppComponent implements OnInit {
   title = 'filter';
+  usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
   userSelect: IUser = {} as IUser;
   showUserDetails: boolean = false;
 
-  usersList: IUser[] = [];
   onUserSelect(user: IUser) {
     this.showUserDetails = true;
     this.userSelect = user;
@@ -22,10 +23,80 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.usersList = UserList;
+      this.usersListFiltered = this.usersList;
     }, 10);
   }
 
   onFilter(filterOptions: IFilterOptions) {
-    console.log(filterOptions);
+    this.usersListFiltered = this.filterUsersList(
+      filterOptions,
+      this.usersList
+    );
+  }
+
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
+    filteredList = this.filterUsersByStatus(filterOptions.status, filteredList);
+
+    filteredList = this.filterUsersByDate(
+      filterOptions.startDate,
+      filterOptions.endDate,
+      filteredList
+    );
+
+    return filteredList;
+  }
+
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+
+    if (NAME_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) =>
+      user.nome.toLowerCase().includes(name.toLowerCase())
+    );
+
+    return filteredList;
+  }
+
+  filterUsersByStatus(
+    status: boolean | undefined,
+    usersList: IUser[]
+  ): IUser[] {
+    const STATUS_NOT_TYPPED = status === undefined;
+    if (STATUS_NOT_TYPPED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.ativo == status);
+
+    return filteredList;
+  }
+
+  filterUsersByDate(
+    startDate: Date | undefined,
+    endDate: Date | undefined,
+    usersList: IUser[]
+  ): IUser[] {
+    const DATES_NOT_SELECTED = startDate === undefined || endDate === undefined;
+
+    if (DATES_NOT_SELECTED) {
+      return usersList;
+    }
+
+    const checkDateInterval = (user: IUser) =>
+      isWithinInterval(new Date(user.dataCadastro), {
+        start: startDate,
+        end: endDate,
+      });
+
+    const listFiltered = usersList.filter(checkDateInterval);
+
+    return listFiltered;
   }
 }
